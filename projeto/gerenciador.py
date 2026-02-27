48ea7a3fde7746b9093f8cdbe6696596ecaf7abc
Willian A Souza <willianarruda052@gmil.com>
2026-02-27 15:28:31 -0300

    novo diretorio, e notas de outra maquina

---
+++ projeto/gerenciador.py
@@ -0,0 +1,23 @@
+while True:
+ print("\n Menu do Gerenciador de Tarefas")
+ print("1. Adicionar Tarefa")
+ print("2. Visualizar Tarefas")
+ print("3. Atulizar Tarefa")
+ print("4. Completar Tarefa")
+ print("5. apagar Tafera Comculida")
+ print("6. Encerar Programa")
+
+ escolha = input("Escolha uma opção: ")
+ if escolha == "1":
+     print("1")
+ if escolha == "2":
+    print("2")
+ if escolha == "3":
+    print("3")
+ if escolha == "4":
+    print("4")
+ if escolha == "5":
+    print("5")
+ if escolha == "6":
+    print("Fin do Programa!")
+    break
