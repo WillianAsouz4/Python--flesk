dae533cc42c77be62b741ca2ed385ca9996c710b
Willian A Souza <willianarruda052@gmail.com>
2026-02-27 11:03:29 -0300

    Notas do primerio modulo

---
+++ boleanos.py
@@ -0,0 +1,29 @@
+#condição falsa
+#True
+#condição verdadeira
+#False
+#condicional
+
+if True:
+    print("bloco IF vai ser executado")
+else:
+    print("bloco else vai ser executado")
+
+#Operadores logico: end e or
+# and
+if True and True:
+    print("Vai executar")
+
+if True and True:
+    print("Não vai executar")
+
+if True and False:
+    print("Vai Rolar Sexo")
+
+
+if True or False:
+    print("Vai executar")
+
+    if True or False:
+        print("Não vai Rolar zexo")
+
