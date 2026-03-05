977b6b3a243bc129e01273eb6aad3d119512097e
Willian A Souza <willianarruda052@gmail.com>
2026-03-05 12:17:38 -0300

    aula 40

---
+++ Mudulo 2/poo.py
@@ -0,0 +1,8 @@
+# POO
+#   é uma paradigma da programacao que tem como objetivo organizar o código em objetos,
+#  onde cada objeto é uma instância de uma classe. A classe é um molde para criar objetos
+#  onde podemos definir atributos e métodos.
+
+class Pessoa:
+    def __init__(self) -> None:
+        pass
--- notas.md
+++ notas.md
@@ -9,4 +9,4 @@ Projeto
 
 source .venv/bin/activate
 
-dia 4 de mar as 00:39 parei na 36 continuar da 37
+Ver aula 40
